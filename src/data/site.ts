5197d5919ff02906036f5b797f7729813c6d44bc
ishimurayuuma <yuuma.i17green@gmail.com>
2026-06-08 17:49:30 +0900

    Set production domain to kabuerabi.com

--- src/data/site.ts
+++ src/data/site.ts
@@ -1,7 +1,7 @@
 export const siteConfig = {
   name: "株選びナビ",
   description: "初心者から中上級者まで使える株・投資の比較サイト",
-  siteUrl: "https://yuuma465.github.io/kabu-erabi-navi/",
+  siteUrl: "https://kabuerabi.com/",
   lineUrl: "https://line.me/R/ti/p/@example",
   copyright: "Copyright © 2026 株選びナビ. All rights reserved."
 };
